8e0f1cec0bf2f8ce9c95dab12207bfea2ebfec0a
artyr231280 <artyr231280@mail.ru>
2024-07-07 16:45:12 +0500

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -4,4 +4,6 @@ count_hour = 1.5
 print(count_hour)
 name_course = 'python'
 print(name_course)
-print(count_hour / count_homework)
+time_work = (count_hour / count_homework)
+print(time_work)
+print('Всего задач:', count_homework , ', затрачено часов', count_hour, ', название курса:', name_course, ', среднее время выполнения:', time_work)
